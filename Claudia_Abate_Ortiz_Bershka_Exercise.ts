a9453419fc3a714c81e45fe025a254d21df51283
Claudia Abate <claudia.abate98@gmail.com>
2025-06-18 16:53:01 +0200

    feat(Bershka_Exercise): add files

---
+++ Claudia_Abate_Ortiz_Bershka_Exercise.ts
@@ -0,0 +1,57 @@
+interface Category {
+  name: string;
+  subcategories: Category[];
+}
+
+type Categories = Category[];
+
+const categories: Categories = [
+  {
+    name: "category1",
+    subcategories: [
+      {
+        name: "category2",
+        subcategories: [],
+      },
+      {
+        name: "category3",
+        subcategories: [
+          {
+            name: "category4",
+            subcategories: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "category5",
+    subcategories: [],
+  },
+];
+
+/**
+ * Returns a path based on the category name and the structure of the categories array
+ * @param {Categories} categories
+ * @param {string} categoryName
+ * @returns {(string|null)} String if the path is found, null if not found
+ */
+const getCategoryPath = (
+  categories: Categories,
+  categoryName: string
+): string | null => {
+  for (const category of categories) {
+    const { name, subcategories } = category;
+
+    if (name === categoryName) return `/${name}`;
+
+    const subPath = getCategoryPath(subcategories, categoryName);
+    if (subPath) return `/${name}${subPath}`;
+  }
+  return null;
+};
+
+// OUTPUT SAMPLES
+console.log(getCategoryPath(categories, "category4")); // should output: '/category1/category3/category4'
+console.log(getCategoryPath(categories, "category2")); // should output: '/category1/category2'
+console.log(getCategoryPath(categories, "category5")); // should output: '/category5'
